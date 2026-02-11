d7e0ce44247c73a0261b121c95941e29bffa2792
elaa <elaa.brahmii@gmail.com>
2026-02-11 18:41:34 +0100

    add db connection + simple app .js main file

---
+++ models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  githubId: { type: String, required: true, unique: true },
+  email: String,
+  name: String,
+  accessToken: String,
+  lastLogin: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('User', UserSchema);
