3c9f28bae3c240c73ce7f0960f50c7cca2aa1207
elaa <elaa.brahmii@gmail.com>
2026-02-17 21:35:16 +0100

    setup monitoring

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 require('dotenv').config();
+const client = require('prom-client');
 const express = require('express');
 const connectDB = require('./config/db');
 const authRouter = require('./routers/authRouter');
 const repoRouter = require('./routers/repoRouter');
 const app = express();
 const cors = require('cors');
+//  INITIALIZE METRICS
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics({ register: client.register });
+
+const httpRequestCounter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
+  labelNames: ['method', 'route', 'status']
+});
+
+app.use((req, res, next) => {
+  res.on('finish', () => {
+    // This records EVERY request that hits your server
+    httpRequestCounter.labels(req.method, req.path, res.statusCode).inc();
+  });
+  next();
+})
+
 
 app.use(cors({
   origin: process.env.FRONTEND_URL, 
@@ -13,7 +32,10 @@ app.use(cors({
 connectDB();
 
 app.use(express.json());
-
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
 app.get('/health', (req, res) => {
   res.send('API is running and DB is connected...');
 });
